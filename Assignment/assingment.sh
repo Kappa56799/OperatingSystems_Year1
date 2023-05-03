d4e893be2863d7f79e8364b8308e17722e922513
Kacper Palka <114831362+Kappa56799@users.noreply.github.com>
2023-05-03 11:05:51 +0000

    assignment

---
+++ Assignment/assingment.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+while true;
+do
+  set -o history
+
+
+  echo "\n* Bash Commands *"
+  echo "1. List Files"
+  echo "2. Show Free disk space"
+  echo "3. Show System path"
+  echo "4. Display command history"
+  echo "5. Backup Files"
+  echo "6. Exit"
+
+  read input
+
+  case $input in
+    1) echo "These are the current files in your directory:"; ls -a; echo;;
+    2) echo "This is the amount of disk space you have left:"; df -h; echo;;
+    3) echo "Your system path is:"; echo $PATH; echo;;
+    4) echo "Your command history is:"; history ;echo ;;
+    5) echo "Enter your directory you want to backup in format /your/directory/here: "; read directory; mkdir ~/BackupFolder;cd $directory; cp -r $directory ~/BackupFolder; ls -a ;echo;;
+    6) exit 1;;
+    *) echo "Invalid Input Try Again"; echo;;
+  esac
+done
